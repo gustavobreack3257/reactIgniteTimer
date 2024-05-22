9ce27132ede8bf6bfc8e494739e8ca8797c65595
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-05-22 08:39:48 -0300

    feat:create History and Home - deleted button component - using routes with react doom

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { ThemeProvider } from "styled-components";
-import { Button } from "./components/Button";
+import { ThemeProvider } from 'styled-components'
 
-import { defaultTheme } from "./styles/theme/default";
+import { defaultTheme } from './styles/theme/default'
+import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 export function App() {
   return (
-    <>
-      <ThemeProvider theme={defaultTheme}>
-        <Button variant="primary" />
-        <Button variant="secondary" />
-        <Button variant="danger" />
-        <Button variant="success" />
-      </ThemeProvider>
-    </>
-  );
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
+  )
 }
