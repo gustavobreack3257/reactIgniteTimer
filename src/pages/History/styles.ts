8915c6b1fb13b964e6fe84aa0cd71118eee2c36d
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-06-03 10:40:01 -0300

    feat: Component Header

---
+++ src/pages/History/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const HomeContainer = styled.main``
+
+export const FormContainer = styled.main``
+export const CountDownContainer = styled.main``
+export const Separator = styled.main``
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,46 @@
+import {
+  HomeContainer,
+  FormContainer,
+  CountDownContainer,
+  Separator,
+  TaskInput,
+  MinutesAmountInput,
+  StartCountdownButton,
+} from './styles'
+
+import { Play } from '@phosphor-icons/react'
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+          />
+
+          <span>minutos</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
