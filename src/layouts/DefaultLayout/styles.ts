23637f1a5ca6d20adcf00b3c82cf591fa4f3f578
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-06-07 10:06:03 -0300

    feat: react-hook-form and zod, hookform- resolvers - validation form and reset form, typescript-form

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
   max-width: 74rem;
-  height: calc(100vh - 10rem);
+  height: calc(100vh - 5rem);
   margin: 5rem auto;
   padding: 2.5rem;
 
@@ -11,4 +11,5 @@ export const LayoutContainer = styled.div`
 
   display: flex;
   flex-direction: column;
+  overflow: auto;
 `
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,6 +12,17 @@ export const GlobalStyle = createGlobalStyle`
         box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
     }
 
+    ::-webkit-scrollbar {
+    width: 6px; /* Largura da barra de rolagem vertical */
+    height: 6px; /* Altura da barra de rolagem horizontal */
+
+}
+
+::-webkit-scrollbar-thumb {
+    background-color:  ${(props) => props.theme['gray-700']};
+    border-radius: 8px;
+}
+
     body {
         background: ${(props) => props.theme['gray-900']};
         color: ${(props) => props.theme['gray-300']};
