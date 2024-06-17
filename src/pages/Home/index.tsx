05bc5d6b2f17f5c49d54af256690c2905891f6ec
Gustavo Costa Souza <drt65211@MAC432701-M.local>
2024-06-17 10:24:13 -0300

    feat: reducing the countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,11 +9,12 @@ import {
 } from './styles'
 
 import { Play } from '@phosphor-icons/react'
-import { useForm } from 'react-hook-form'
+import { differenceInSeconds } from 'date-fns'
 
+import { useForm } from 'react-hook-form'
+import { useEffect, useState } from 'react'
 import * as zod from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useState } from 'react'
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, 'Informe a tarefa'),
@@ -29,6 +30,7 @@ interface Cycle {
   id: string
   task: string
   minutesAmount: number
+  startDate: Date
 }
 
 export function Home() {
@@ -44,6 +46,16 @@ export function Home() {
     },
   })
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  useEffect(() => {
+    setInterval(() => {
+      setAmountSecondsPassed(
+        differenceInSeconds(new Date(), activeCycle.startDate),
+      )
+    }, 1000)
+  }, [activeCycle])
+
   function handleCreteCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime())
 
@@ -51,6 +63,7 @@ export function Home() {
       id,
       task: data.task,
       minutesAmount: data.minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles((state) => [...state, newCycle])
@@ -59,7 +72,6 @@ export function Home() {
     reset()
   }
 
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0
 
@@ -68,6 +80,7 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0')
   const seconds = String(secondsAmount).padStart(2, '0')
+
   const task = watch('task')
   const isSubmitDisable = !task
 
